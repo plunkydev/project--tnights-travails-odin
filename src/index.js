89ca975da829e298588fbdc9b0e63c114f6498c0
plunkydev <plunkydesign@gmail.com>
2025-09-25 10:42:04 -0500

    test: add multiple console.log examples using findShortestPath with knightMoves

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { knightMoves } from "./knightMoves";
+import findShortestPath from "./utils/findShortestPath"
 
-console.log(knightMoves([3,3]))
+console.log(findShortestPath([0,0], [1,2], knightMoves))
+console.log(findShortestPath([0,0], [3,3], knightMoves))
+console.log(findShortestPath([3,3], [0,0], knightMoves))
+console.log(findShortestPath([0,0], [7,7], knightMoves))
