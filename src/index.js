18c3fd6320ee77ad83837872554a84dd14f6d8a2
plunkydev <plunkydesign@gmail.com>
2025-09-25 10:47:18 -0500

    build: 1.0.0

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { knightMoves } from "./knightMoves";
-import findShortestPath from "./utils/findShortestPath"
-
-console.log(findShortestPath([0,0], [1,2], knightMoves))
-console.log(findShortestPath([0,0], [3,3], knightMoves))
-console.log(findShortestPath([3,3], [0,0], knightMoves))
-console.log(findShortestPath([0,0], [7,7], knightMoves))
